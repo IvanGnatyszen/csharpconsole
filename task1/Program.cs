b27ac85ab180cf9f20d9a41b845aec1decae5fdf
IvanGnatyszen <92626023+IvanGnatyszen@users.noreply.github.com>
2022-09-12 16:15:46 +0200

    Add files via upload

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Podaj imie: ");
+string name = Console.ReadLine();
+Console.Write("Podaj nazwisko: ");
+string surename = Console.ReadLine();
+Console.Write("Podaj wiek: ");
+int age = Convert.ToInt32(Console.ReadLine());
+string promoCardType;
+
+DateTime localDate = DateTime.Now;
+
+if(age > 17){
+    promoCardType = "Promo card for adult: \n";
+} else {
+    promoCardType = "Promo card for youth: \n";
+}
+
+// Console.WriteLine(promoCardType + "name: " + name + "\n surename: " + surename + "\n age: " + age);
+Console.WriteLine($"{promoCardType} name: {name} \n surename: {surename} \n age: {age} \nActual date is {localDate.ToShortDateString()}");
